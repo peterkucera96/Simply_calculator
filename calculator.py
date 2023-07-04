090b461f536189ed136dcbe0f7c93d10135e4875
Peter Kučera <peter44488@gmail.com>
2023-07-04 10:57:10 +0200

    adjusting the calculator

--- calculator.py
+++ calculator.py
@@ -1,18 +1,43 @@
-# Vytvorenie zakladov kalkualcky
+# Program kalkulacky
 print("Vitajte v kalkulacke")
-operator = input("Vyberte operator(+ - * /): ")
-cislo1 = float(input('Zadaj cislo 1: '))
-cislo2 = float(input('Zadaj cislo 2: '))
+opakovanie = True
+while opakovanie:
+    operator = None
+    while operator not in ['+', '-', '*', '/']:
+        operator = input("Vyberte operator (+ - * /): ")
+        if operator not in ['+', '-', '*', '/']:
+            print('Chyba: Zadali ste nespravny operator!')
+
+    
+    while True:
+        try:
+            cislo1 = float(input("Zadajte číslo 1: "))
+            break
+        except ValueError:
+            print("Chyba: Zadali ste nesprávne číslo!")
+
+    while True:
+        try:
+            cislo2 = float(input("Zadajte číslo 2: "))
+            break
+        except ValueError:
+            print("Chyba: Zadali ste nesprávne číslo!")
 
 
-# vetvenie pomocou if
-if operator == '+':
-    print('Sucet je: ', cislo1 + cislo2)
-elif operator == '-':
-    print('Rozdiel je: ', cislo1 - cislo2)
-elif operator == '*':
-    print('Sucin je: ', cislo1 * cislo2)
-else:
-    print('Deleno je: ', cislo1 / cislo2)
+    if operator == '+':
+        print('Vysledok je: ', cislo1 + cislo2)
+    elif operator == '-':
+        print('Vysledok je: ', cislo1 - cislo2)
+    elif operator == '*':
+        print('Vysledok je: ', cislo1 * cislo2)
+    elif operator == '/':
+        if cislo1 != 0:
+            print('Cislom 0 sa neda delit.')
+        else:
+            print('Vysledok je: ', cislo1 / cislo2)
 
+    pokracovat = input("Chcete pokračovať? (Y/N): ")
+    if pokracovat.upper() != "Y":
+        opakovanie = False
 
+print('Dakujem ze ste pouzili kalkulacku.')
